9796077fa7a1a688ecfd03788fd546635ce0d17b
rautavinash2811 <rautavinash2811@gmail.com>
2025-04-15 16:55:46 +0530

    finalised

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for
 import psycopg2
 app = Flask(__name__)
+
 # @app.route('/')
 # def main():
 #     #name = input("Enter the name of student : ")
